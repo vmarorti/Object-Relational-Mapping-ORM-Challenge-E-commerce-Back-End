3ac544649c36750ecd7179139fa407bfa7b579d8
vmarorti <marsmotionless98@gmail.com>
2024-10-10 06:32:39 -0500

    Finished category-routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,71 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const data = await Category.findAll({
+      include: [Product]
+    })
+    res.json(data)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const data = await Category.findByPk(req.params.id, {
+      include: [Product]
+    })
+    res.json(data)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const data = await Category.create(req.body)
+    res.json(data)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const category = await Category.findByPk(req.params.id);
+    if (!category) {
+      return res.status(404).json({ error: 'Category not found'});
+    }
+
+    category.category_name = req.body.category_name;
+    await category.save();
+    res.json(category);
+  } catch (err) {
+    res.status(500).json({ error: err.message});
+  }
 });
 
-router.delete('/:id', (req, res) => {
+
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const data = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.json(data)
+  } catch (err) {
+    res.status(500).json(err.message)
+  }
 });
 
 module.exports = router;
